b279946c77abdaa9c096ce0dee2fc3aba95c3eaf
moo <34907770+moo-the-cow@users.noreply.github.com>
2026-04-21 19:23:47 +0200

    added autosetup bat files to upgrade

--- windows/upgrade.bat
+++ windows/upgrade.bat
@@ -6,6 +6,8 @@ setlocal enabledelayedexpansion
 :: ============================================================
 set "STATS_SERVER_URL=https://github.com/moo-the-cow/moo-rist-hosting-native/raw/refs/heads/main/windows/StatsServer.zip"
 set "LIBRIST_URL=https://github.com/moo-the-cow/moo-rist-hosting-native/raw/refs/heads/main/windows/librist.zip"
+set "HOME_BAT_URL=https://github.com/moo-the-cow/moo-rist-hosting-native/raw/refs/heads/main/windows/autosetup_home.bat"
+set "REMOTE_BAT_URL=https://github.com/moo-the-cow/moo-rist-hosting-native/raw/refs/heads/main/windows/autosetup_remote.bat"
 :: ============================================================
 
 echo Upgrading...
@@ -27,6 +29,13 @@ powershell -Command "Invoke-WebRequest -Uri '%LIBRIST_URL%' -OutFile 'librist.zi
 tar -xf "librist.zip"
 echo Replaced: librist\tools\ristreceiver.exe
 echo Replaced: librist\tools\ristsender.exe
+
+:: Download additional .bat scripts
+powershell -Command "Invoke-WebRequest -Uri '%HOME_BAT_URL%' -OutFile 'autosetup_home.bat'"
+echo Replaced: autosetup_home.bat
+
+powershell -Command "Invoke-WebRequest -Uri '%REMOTE_BAT_URL%' -OutFile 'autosetup_remote.bat'"
+echo Replaced: autosetup_remote.bat
 
 :: Cleanup
 :: del "StatsServer.zip" >nul
